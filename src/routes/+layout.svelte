a9d1532eac9fdc2623ae4500c7d69cbe48482ea0
zenzak-uk <zenzak@tuta.io>
2025-08-12 19:28:26 +0100

    poster.avif

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,9 +31,9 @@
 	<meta property="twitter:image" content="https://www.zenzak.uk/social-preview.png" />
 </svelte:head>
 
-<div class="app-container" style="--poster-url: url({base}/videos/poster.jpg)">
+<div class="app-container" style="--poster-url: url({base}/videos/poster.avif)">
 	<div class="background-video-container">
-		<video autoplay muted loop playsinline poster="{base}/videos/poster.jpg">
+		<video autoplay muted loop playsinline poster="{base}/videos/poster.avif">
 			<source src="{base}/videos/bg.mp4" type="video/mp4" />
 			Your browser does not support the video tag.
 		</video>
