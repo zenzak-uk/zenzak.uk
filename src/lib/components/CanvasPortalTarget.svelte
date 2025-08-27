c9e84b7910ee30b872fcd0fea052e34660e70b0d
zenzak-uk <zenzak@tuta.io>
2025-08-27 18:57:59 +0100

    Add files via upload

---
+++ src/lib/components/CanvasPortalTarget.svelte
@@ -0,0 +1,17 @@
+<script
+  lang="ts"
+  module
+>
+  import type { Snippet } from 'svelte'
+  import { SvelteSet } from 'svelte/reactivity'
+  let snippets = new SvelteSet<Snippet>()
+  export const addCanvasPortalSnippet = (snippet: Snippet) => {
+    snippets.add(snippet)
+  }
+  export const removeCanvasPortalSnippet = (snippet: Snippet) => {
+    snippets.delete(snippet)
+  }
+</script>
+{#each snippets as snippet}
+  {@render snippet()}
+{/each}
