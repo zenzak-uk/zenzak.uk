c36312185eddf3f31d06786909116e64893e977c
zenzak-uk <zenzak@tuta.io>
2025-08-29 15:07:31 +0000

    about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  // Depth3D example
+  import Depth3D from '$lib/Depth3D';
+</script>
+
+<!-- will adjust to the parents size/position -->
+<Depth3D
+	image={{
+		image: '/images/me.webp',
+		depth: '/images/depth.png'
+	}}
+/>
